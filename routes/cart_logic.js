d7ae19a440f397c7c6cebdce00f7a247b4377684
JFMart <Frank@Joses-MacBook-Pro.local>
2013-10-06 03:26:57 -0400

    Adding checkout

--- routes/cart_logic.js
+++ routes/cart_logic.js
@@ -1,5 +1,5 @@
 
-var cart_items =
+var user_data =
 {
 	content:[
 	{
@@ -8,23 +8,47 @@ var cart_items =
 		description: "Baby sloth up for adoption, sign in your papers today!",
 		image: "http://goo.gl/ffICmz",
 		price: 40
-			},
+	},
 		{
 
 		name: "iWallet",
 		description: "New and improved Apple Leather iWallet",
-		image: "http://goo.gl/h6GYBh",
+		image: "http://goo.gl/uMaaZq",
 		price: 210
 	}
+	],
+	address:
+	[
+	{
+		address_one: "Calle Reina Maria 139",
+		address_two: "Urb. La Villa de Torrimar",
+		city: "Guaynabo",
+		zipcode: "00969",
+		country: "Puerto Rico"
+	}
 
+	],
+	credit_cards:
+	[
+		{
+			credit_card_number: 1234565687871929,
+			cardholder: "John Smith",
+			expiration_date: "08/16",
+			type: "Visa"
 
-
+		}
 	]
 };
 
+
+var get_address = function(req,res, err)
+{
+	res.send(user_data);
+}
 var get_cart = function(req, res, err)
 {
-	res.send(cart_items);
+	res.send(user_data);
 }
 
 exports.get_cart = get_cart;
+exports.get_address = get_address;
