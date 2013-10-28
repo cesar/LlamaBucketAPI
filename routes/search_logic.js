fd5df969a41b93c6e302d140fe98e0bf8f64d034
cesarcruz91 <cesarcruz91@gmail.com>
2013-10-28 16:11:44 -0400

    Finished MySQL support for the search_logic

--- routes/search_logic.js
+++ routes/search_logic.js
@@ -1,6 +1,223 @@
 /*
 *	Ideally this is how the items will be passed to the client side.
 */
+<<<<<<< Updated upstream
+=======
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host : process.env.CLEARDB_DATABASE_URL,  //Set up the database connection host
+  user : process.env.CLEARDB_DATABASE_USERNAME, //Username
+  password : process.env.CLEARDB_DATABASE_PASSWORD,  //Password
+  database : process.env.CLEARDB_DATABASE,  //database name
+});
+
+
+var items = {
+	content : [
+	{ id : 0,
+	  name : "Sword",
+	  description: "An authentic samurai sword, from acient Japan",
+	  year : 09,
+	  brand : "AJB",
+	  general: "Don't know what goes here",
+	  category : 7,
+	  price : 1.30,
+	  image : "http://images4.wikia.nocookie.net/__cb20130105173711/runescape/images/c/cb/Steel_sword_detail.png"
+	},
+	{ id : 1,
+	  name : "Kick Ass Ball",
+	  description: "This is one kickass ball",
+	  year : 09,
+	  brand : "CoolShiii",
+	  general: "Don't know what goes here",
+	  category : 7,
+	  price : 2.35,
+	  image : "http://static.giantbomb.com/uploads/scale_small/0/6393/528516-1ball2.jpg"
+	},
+	{ id : 2,
+	  name : "Toy Car",
+	  description: "A toy car with some stolen tires",
+	  year : 09,
+	  brand : "MadFos",
+	  general: "Don't know what goes here",
+	  category : 7,
+	  price : 2.23,
+	  image : "http://img.dooyoo.co.uk/GB_EN/orig/0/8/4/4/9/844993.jpg"
+	},
+	{ id : 3,
+	  name : "Nexus 4",
+	  description: "Because it's awesome!",
+	  year : 09,
+	  brand : "Google",
+	  general: "Don't know what goes here",
+	  category : 1,
+	  price : 250.00,
+	  image : "http://www.notebookcheck.net/fileadmin/_migrated/pics/nexus4-1_02.png"
+	},
+	{ id : 4,
+	  name : "iPhone 5",
+	  description: "Aww yeah, it's an iPhone",
+	  year : 09,
+	  brand : "Apple",
+	  general: "Don't know what goes here",
+	  category : 1,
+	  price : 570.99,
+	  image : "http://cdn.arstechnica.net/wp-content/uploads/2012/09/iphone5-intro.jpg"
+	},
+	{ id : 5,
+	  name : "YouWantIt",
+	  description: "You don't know what this is, but you want it.",
+	  year : 09,
+	  brand : "YouWantIt.Com",
+	  general: "Don't know what goes here",
+	  category : 5,
+	  price : 0.01,
+	  image : "http://cdn.bulbagarden.net/upload/thumb/7/7e/006Charizard.png/256px-006Charizard.png"
+	},
+	{ id : 6,
+	  name : "Harry Potter: The Prisoner of Azkaban",
+	  description: "Third Harry Potter book.",
+	  year : 09,
+	  brand : "Schoolastic",
+	  general: "Don't know what goes here",
+	  category : 9,
+	  price : 0.01,
+	  image : "http://upload.wikimedia.org/wikipedia/en/a/a0/Harry_Potter_and_the_Prisoner_of_Azkaban.jpg"
+	},
+	{ id : 7,
+	  name : "Enders Game",
+	  description: "This is a cool book I guess.",
+	  year : 09,
+	  brand : "YouWantIt.Com",
+	  general: "Don't know what goes here",
+	  category : 9,
+	  price : 0.01,
+	  image : "http://bibliomantics.files.wordpress.com/2012/01/enders-game.jpg"
+	},
+	{ id : 8,
+	  name : "The Kanye Shirt",
+	  description: "A white shirt, from the Kanye line of clothing",
+	  year : 09,
+	  brand : "YouWantIt.Com",
+	  general: "Don't know what goes here",
+	  category : 23,
+	  price : 0.01,
+	  image : "http://www.glamour.com/images/fashion/2013/07/kanye-west-white-t-shirt-w724.jpg"
+	},
+	{ id : 9,
+	  name : "Funny Shirt",
+	  description: "This is one funny shirt",
+	  year : 09,
+	  brand : "YouWantIt.Com",
+	  general: "Don't know what goes here",
+	  category : 23,
+	  price : 0.01,
+	  image : "http://www.adamedesigns.com/wp-content/uploads/2013/07/poopedtoday.jpg"
+	},
+	{ id : 10,
+	  name : "LG TV",
+	  description: "A TV",
+	  year : 09,
+	  brand : "YouWantIt.Com",
+	  general: "Don't know what goes here",
+	  category : 12,
+	  price : 0.01,
+	  image : "http://www.opb-equipment.com/images/categories/lg-tv.jpg"
+	},
+	{ id : 11,
+	  name : "Xerox Printer",
+	  description: "This printer, you will love",
+	  year : 09,
+	  brand : "YouWantIt.Com",
+	  general: "Don't know what goes here",
+	  category : 20,
+	  price : 0.01,
+	  image : "http://www.fujixeroxprinters.com.au/downloads/uploaded/DocuPrint%20CM205%20L%20closed_6330.jpg"
+	},
+	{ id : 12,
+	  name : "Apple iPad",
+	  description: "You don't know what this is, but you want it.",
+	  year : 09,
+	  brand : "YouWantIt.Com",
+	  general: "Don't know what goes here",
+	  category : 19,
+	  price : 0.01,
+	  image : "http://www.technobuffalo.com/wp-content/uploads/2012/12/ipad-mini-scaled-1.jpg"
+	},
+	{ id : 13,
+	  name : "Windows Surface",
+	  description: "Crappy tablet, cause windows 8.",
+	  year : 09,
+	  brand : "YouWantIt.Com",
+	  general: "Don't know what goes here",
+	  category : 19,
+	  price : 0.01,
+	  image : "http://www.xda-developers.com/wp-content/uploads/2013/01/MSSurface.jpg"
+	},
+	{ id : 14,
+	  name : "Schiwm Bike Frame",
+	  description: "Original replacement",
+	  year : 09,
+	  brand : "YouWantIt.Com",
+	  general: "Don't know what goes here",
+	  category : 34,
+	  price : 0.01,
+	  image : "http://www.selectism.com/news/wp-content/uploads/2008/11/schwinn-paramount-70-anniversary-frame-front.jpg"
+	},
+	{ id : 15,
+	  name : "Color Socks",
+	  description: "For you, you hipster",
+	  year : 09,
+	  brand : "YouWantIt.Com",
+	  general: "Don't know what goes here",
+	  category : 25,
+	  price : 0.01,
+	  image : "http://4.bp.blogspot.com/_KEDScn-6OGA/THe_8MpGhRI/AAAAAAAADTQ/GywfyfoM2e0/s1600/knee_high_striped_socks_bright_colors.jpg"
+	},
+	{ id : 16,
+	  name : "Rebel Shirt",
+	  description: "Something goes here",
+	  year : 09,
+	  brand : "YouWantIt.Com",
+	  general: "Don't know what goes here",
+	  category : 27,
+	  price : 0.01,
+	  image : "http://www3.images.coolspotters.com/photos/865160/princess-leia-rebel-t-shirt-profile.jpg"
+	},
+	{ id : 17,
+	  name : "Callaway Golf Set",
+	  description: "Signed by yo mama",
+	  year : 09,
+	  brand : "YouWantIt.Com",
+	  general: "Don't know what goes here",
+	  category : 41,
+	  price : 0.01,
+	  image : "http://ecx.images-amazon.com/images/I/51yPsp6e1EL.jpg"
+	},
+	{ id : 18,
+	  name : "Suitcase",
+	  description: "You'll get that job, trust us.",
+	  year : 09,
+	  brand : "YouWantIt.Com",
+	  general: "Don't know what goes here",
+	  category : 5,
+	  price : 0.01,
+	  image : "http://1.bp.blogspot.com/_MWG4tkTfz8M/SwvRjlHN66I/AAAAAAAAAVs/nKIUTHnKsFI/s320/Vintage_Suitcase_-_V%26M.jpg"
+	},
+	{ id : 19,
+	  name : "MacBookPro Retina",
+	  description: "Yes.",
+	  year : 09,
+	  brand : "YouWantIt.Com",
+	  general: "Don't know what goes here",
+	  category : 17,
+	  price : 0.01,
+	  image : "http://i.zdnet.com/blogs/macbook-pro-retina-display-ogrady.jpg"
+	}]
+}
+>>>>>>> Stashed changes
 
 var mysql = require('mysql');
 
@@ -60,12 +277,20 @@ var get_results = function(req, res, next)
 var get_categories = function(req, res, next)
 {
 
+<<<<<<< Updated upstream
 connection.query('select * from category', function(err, rows){
 	if(!err)
 		res.send({content : rows});
 	else
 		console.log(err);
 });
+=======
+connection.query('SELECT * FROM test', function(err, rows){
+  console.log(rows);
+  console.log(err);
+});
+	res.send(categories);
+>>>>>>> Stashed changes
 }
 
 
