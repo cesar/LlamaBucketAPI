f7bd2c562ef45c08c05fa5b38708db668dc1dee0
Luis Medina <luis.medina14@upr.edu>
2013-11-08 18:00:07 -0400

    User list can be get now from the DB, reports soon.

--- routes/admin_logic.js
+++ routes/admin_logic.js
@@ -1,76 +1,29 @@
-//Some generic user information, loosely based on the information the app would get from the DB.
-var users = {
-	content : [
-		{
-		userId : 0,
-		firstname : "Cesar",
-		lastname : "Cruz",
-		email: "cesar.cruz5@upr.edu",
-		phone : "1234567890", 
-		address : "Somewhere",
-		isAdmin : 1
-		},
-		{
-		userId : 1,
-		firstname : "Luis",
-		lastname : "Medina",
-		email: "luis.medina14@upr.edu",
-		phone : "1234567890", 
-		address : "Over",
-		isAdmin : 1
-		},
-		{
-		userId : 2,
-		firstname : "Jose",
-		lastname : "Martinez",
-		email: "jose.martinez60@upr.edu",
-		phone : "1234567890", 
-		address : "The",
-		isAdmin : 1
-		},
-		{
-		userId : 3,
-		firstname : "Pepa",
-		lastname : "Jimenez",
-		email: "locolocalokita@aol.com",
-		phone : "1234567890", 
-		address : "Rainbow",
-		isAdmin : 0
-		}]
-	}
+var mysql = require('mysql');
 
-//Not representative of an official report. Mainly used to show functionality of the report display.
-var report = {
-	content : [{
-	report_name : "Sales from 31/10/2013",
-	itemA : "Ipad",
-	sellerA : "Pepa",
-	buyerA  : "Fernan",
-	priceA : "200",
-	itemB : "Iphone",
-	sellerB : "Pepa",
-	buyerB : "Fernan",
-	priceB: "200",
-	itemC : "Ipad Mini",
-	sellerC : "Fernan",
-	buyerC : "Pepa",
-	priceC : "150",
-	itemD : "Ipod Touch",
-	sellerD : "Luis",
-	buyerD : "Jose",
-	priceD : "9999.99"
-
-	}]
-}
+var connection = mysql.createConnection({
+	host : process.env.CLEARDB_DATABASE_URL,  //Set up the database connection host
+  	user : process.env.CLEARDB_DATABASE_USERNAME, //Username
+  	password : process.env.CLEARDB_DATABASE_PASSWORD,  //Password
+  	database : process.env.CLEARDB_DATABASE,  //database name
+});
 
 var get_users = function(req, res, next){
-	res.send(users.content);
+	connection.query('SELECT client_id, client_firstname, client_lastname, email, isAdmin FROM client', function(err, rows){
+		console.log("Getting users from db...");
+		res.send(rows[0]);
+		console.log(rows[0]);
+	});
 }
 var get_individual = function(req, res, next){
-	res.send(users.content[req.params.parameter]);
+	
+		connection.query('SELECT client_id, client_firstname, client_lastname, email, isAdmin FROM client WHERE client_id=' + req.params.parameter, function(err, rows){
+		console.log("Getting users from db...");
+		res.send(rows[0]);
+		console.log(rows[0]);
+	});
 }
 var get_report = function(req, res, next){
-	res.send(report.content);
+	//res.send(report.content);
 }
 exports.get_report = get_report;
 exports.get_individual = get_individual;
