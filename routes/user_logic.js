1196892872f9b88e7882435abd56f8aa53823298
cesarcruz91 <cesarcruz91@gmail.com>
2013-11-08 13:03:10 -0400

    Added sign in functionality, passwords still need to be hashed.

--- routes/user_logic.js
+++ routes/user_logic.js
@@ -14,125 +14,6 @@ var connection = mysql.createConnection({
 });
 
 
-var user = {
-
-
-	first_name : 'César',
-	last_name : 'Cruz',
-	email : 'cesarcruz91@gmail.com',
-	password : 'forkit', //This will be hashed.
-	phone : '7874526702',
-	mail_address1 : '#311 Calle Ext. Los Robles',
-	mail_address2 : '',
-	mail_city : 'Rincon',
-	mail_state : 'Puerto Rico',
-	mail_zip : '00677',
-	bill_address1 : '#311 Calle Ext. Los Robles',
-	bill_address2 : '',
-	bill_city : 'Rincon',
-	bill_state : 'Puerto Rico',
-	bill_zip : '00677',
-	image : 'https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-ash4/1009521_322256844573732_661186218_o.jpg',
-	cc_number : '2938 8493 1739 0394',
-
-	notifications:[
-
-
-	{
-		date: "Oct 03, 2013 - 12:21pm",
-		description: "Your bid on \"Coors Light 12 pack\" was outbidded",
-		link: ""
-
-
-	},
-	{
-		date: "Oct 04, 2013 - 2:01am",
-		description: "You won the bid on the new \"iPad 7\"",
-		link:""
-
-	},
-
-	{
-
-		date: "September 21, 2013 - 12:00am",
-		description: "Credit card ending with \"*0394\" has expired.",
-		link: ""
-	}
-
-
-	],
-
-	bids:[
-	{	
-		date: "October 2, 2013 - 2:10pm",
-		item_name: "Charizard",
-		amount: "$2.00",
-		seller: "Luis Medina",
-		image: "http://cdn.bulbagarden.net/upload/thumb/7/7e/006Charizard.png/256px-006Charizard.png",
-		link: ""
-
-
-
-	},
-
-	{
-		date: "October 1, 2013 - 9:00pm",
-		item_name: "Sword",
-		amount: "$15.00",
-		seller: "Jose Martinez",
-		image: "http://images4.wikia.nocookie.net/__cb20130105173711/runescape/images/c/cb/Steel_sword_detail.png",
-		link: ""
-
-
-
-	},
-
-	{
-		date: "September 19, 2013 - 7:45am",
-		item_name: "Harry Potter: The Prisoner of Azkaban",
-		amount: "$13.50",
-		seller: "John Smith",
-		image: "http://upload.wikimedia.org/wikipedia/en/a/a0/Harry_Potter_and_the_Prisoner_of_Azkaban.jpg",
-		link:""
-	},
-	{
-		date: "September 12, 2013 - 8:45am",
-		item_name: "Suitcase",
-		amount: "$56.00",
-		seller: "Xin Perez",
-		image: "http://1.bp.blogspot.com/_MWG4tkTfz8M/SwvRjlHN66I/AAAAAAAAAVs/nKIUTHnKsFI/s320/Vintage_Suitcase_-_V%26M.jpg",
-		link:""
-	}
-	],
-
-	listings:[
-	{
-
-	  name : "Kick Ass Ball",
-	  description: "This is one kickass ball",
-	  date : "August 2, 2013",
-	  brand : "CoolShiii",
-	  category: "Toys",
-	  price : "Price: $2.35",
-	  image : "http://static.giantbomb.com/uploads/scale_small/0/6393/528516-1ball2.jpg",
-	  auction_flag: false
-
-	},
-	
-
-	{ name : "Nexus 4",
-	  description: "Because it's awesome!",
-	  date : "September 8, 2013",
-	  brand : "Google",
-	  category : "Tablets",
-	  price : "Current Bidding: $253.30",
-	  image : "http://www.notebookcheck.net/fileadmin/_migrated/pics/nexus4-1_02.png",
-	  auction_flag: false
-	}
-	]
-};
-
-
 // User notifications
 
 /*
@@ -140,19 +21,29 @@ var user = {
 */
 var sign_in = function(req, res, next)
 {
+	console.log(req.body);
+	var query = 'select password, client_id from client where email = ' + connection.escape(req.body.email);
 
-	connection.query('select * from client natural join address where client.email = '+connection.escape(req.body.username) +'and address.is_primary = 1', function(err, rows){
+	connection.query(query, function(err, user){
 		if (!err)
-			if(rows[0].password == req.body.password)
-				res.send(rows[0])
+		{
+			console.log(user);
+			if(user[0].password == req.body.password)
+			{
+				var send_data = {
+					id : user[0].client_id
+				}
+				res.send(send_data);
+			}
 			else
-				res.send({error : 'Incorrect'});
+				res.send({error : 'Incorrect Password'});
+		}
+		//User email does not exist on our records as typed.
 		else{
 			console.log(err);
-			res.send({error : 'dumbass'});
+			res.send({error : 'Not Found'});
 		}
-			console.log(err);
-	})
+	});
 }
 
 //Get the user profile
